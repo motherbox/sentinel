e10e72369564e68911f924fabbfc87477e766ec7
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2016-09-23 23:20:59 +0530

    Final touches

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -4,7 +4,38 @@ from bs4 import BeautifulSoup as Bs
 import requests
 import traceback
 import json
-import sys
+import re
+import pandas as pd
+import numpy as np
+
+
+def json2df(fname, year):
+    with open(fname, "r") as f_in:
+        orgData = json.load(f_in)
+    cleanTitles = {}
+    pattern = "[0-9]+\."
+    for k, v in orgData.iteritems():
+        title = re.sub(pattern, "", k.lstrip().rstrip())
+        cleanTitles[title.lstrip().rstrip()] = v
+    df = pd.DataFrame(index=cleanTitles.keys())
+    for key in v.keys():
+        df[key] = ""
+    for ix, rowData in df.iterrows():
+        propData = cleanTitles[ix]
+        for k, v in propData.iteritems():
+            if k != "comments":
+                rowData.loc[k] = v
+    df.columns = [c.lower().replace(" ", "_").replace(":", "") for c in df]
+    df['n_comments'] = df.pop('comments')
+    for ix, rowdata in df.iterrows():
+        rowdata.loc["n_comments"] = len(cleanTitles[ix]['comments'])
+    df['n_comments'] = df['n_comments'].astype(int)
+    df['n_votes'] = df['n_votes'].astype(int)
+    df['year'] = year
+    for col in df:
+        if df[col].dtype is np.dtype('O'):
+            df[col] = df.pop(col).apply(lambda x: x.lstrip().rstrip())
+    return df
 
 
 class Scrape:
@@ -89,6 +120,15 @@ class Scrape:
         return self.result
 
 if __name__ == '__main__':
-    results = Scrape(sys.argv[1]).start()
-    with open(sys.argv[2], "w") as f_out:
+    cfp_2015_url = "https://in.pycon.org/cfp/pycon-india-2015/proposals/"
+    cfp_2016_url = "https://in.pycon.org/cfp/2016/proposals/"
+    results = Scrape(cfp_2015_url).start()
+    with open("cfp2015.json", "w") as f_out:
         json.dump(results, f_out)
+    results = Scrape(cfp_2016_url).start()
+    with open("cfp2016.json", "w") as f_out:
+        json.dump(results, f_out)
+    df1 = json2df("cfp2015.json", 2015)
+    df2 = json2df("cfp2016.json", 2016)
+    df = pd.concat((df1, df2), axis=0)
+    df.to_csv("cfp.tsv", encoding="utf-8", sep="\t", index_label="title")
